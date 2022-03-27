c85fff26bb9c64a0fae12a986cbbb5db21d5087e
Kiril Kaloyanov <kaloyanov.kiril@gmail.com>
2022-03-27 11:20:46 +0300

    Implement joi validation.

--- routes/participants.js
+++ routes/participants.js
@@ -1,10 +1,24 @@
 const express = require("express");
 const router = express.Router();
-const { Participant } = require("../models/participant");
+const { Participant, validateParticipant } = require("../models/participant");
 
 router.post("/", async (req, res) => {
+  const { error } = validateParticipant(req.body);
+  if (error) {
+    const ex = { errors: {} };
+    for (let item of error.details) {
+      ex.errors[item.path[0]] = item.message;
+    }
+    res.send(ex);
+    return;
+  }
+  let participant = await Participant.findOne({ email: req.body.email });
+  if (participant)
+    return res.send({
+      errors: { emailRegistered: "Email already registered" },
+    });
   try {
-    let participant = new Participant({
+    participant = new Participant({
       firstName: req.body.firstName,
       lastName: req.body.lastName,
       email: req.body.email,
@@ -12,8 +26,8 @@ router.post("/", async (req, res) => {
     });
     participant = await participant.save();
     res.send(participant);
-  } catch (err) {
-    res.send(err);
+  } catch (ex) {
+    res.send(ex);
   }
 });
 
